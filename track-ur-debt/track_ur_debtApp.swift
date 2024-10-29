019051aafcc01af62b2da6c9f25c4674ab6c7cf0
apoloniaabramowicz <pola.abramowicz@onet.pl>
2024-10-29 08:40:09 +0100

    Initial Commit

---
+++ track-ur-debt/track_ur_debtApp.swift
@@ -0,0 +1,17 @@
+//
+//  track_ur_debtApp.swift
+//  track-ur-debt
+//
+//  Created by Apolonia Abramowicz on 29/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct track_ur_debtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ track-ur-debtTests/track_ur_debtTests.swift
@@ -0,0 +1,17 @@
+//
+//  track_ur_debtTests.swift
+//  track-ur-debtTests
+//
+//  Created by Apolonia Abramowicz on 29/10/2024.
+//
+
+import Testing
+@testable import track_ur_debt
+
+struct track_ur_debtTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
